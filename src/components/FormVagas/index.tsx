fd7aec567416a1910e6bd4e56c6b9ffc209d3a32
SamSepi0llll <david.bonardi@hotmail.com>
2024-08-22 16:58:22 -0300

    Finishing the project

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,7 +1,39 @@
 import { FormEvent, useState } from 'react'
+import styled from 'styled-components'
 
-import styles from './FormVagas.module.css'
-import Form from './Form'
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: var(--cor-secundaria);
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+`
+
+const FormInput = styled.input`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+`
+
+const ButtonSearch = styled.button`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;
+  border-radius: 5px;
+
+  @media (max-width: 768px) {
+    padding: 0 8px;
+  }
+`
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -16,17 +48,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Form onSubmit={aoEnviarForm}>
+      <FormInput
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <ButtonSearch type="submit">Pesquisar</ButtonSearch>
+    </Form>
   )
 }
 export default FormVagas
